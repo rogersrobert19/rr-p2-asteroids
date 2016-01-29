cc704e8c5bfa63ea03a29804e3717e0fd054f073
robertr <roboodaboo16@gmail.com>
2016-01-28 23:40:01 -0800

    Initial commit
    moving most recent version to git

---
+++ Asteroid.java
@@ -0,0 +1,97 @@
+import greenfoot.*;
+
+/**
+ * A rock in space.
+ * 
+ * @author Poul Henriksen
+ * @author Michael Kölling
+ */
+public class Asteroid extends SmoothMover
+{
+    /** Size of this asteroid */
+    private int size;
+
+    /** When the stability reaches 0 the asteroid will explode */
+    private int stability;
+
+
+    /**
+     * Create an asteroid with default size and random direction of movement.
+     */
+    public Asteroid()
+    {
+        this(50);
+    }
+    
+    /**
+     * Create an asteroid with a given size and random direction of movement.
+     */
+    public Asteroid(int size)
+    {
+        super(new Vector(Greenfoot.getRandomNumber(360), 2));
+    }
+    
+    /**
+     * Create an asteroid with a given size and direction of movement.
+     */
+    public Asteroid(int size, Vector velocity)
+    {
+        super(velocity);
+        //changeimage(size);
+    }
+    
+    public void act()
+    {         
+        move();
+    }
+
+    /**
+     * Set the size of this asteroid. Note that stability is directly
+     * related to size. Smaller asteroids are less stable.
+     *
+    public void setSize(int size) 
+    {
+        stability = size;
+        this.size = size;
+        GreenfootImage image = getImage();
+        image.scale(size, size);
+    }
+
+    /**
+     * Return the current stability of this asteroid. (If it goes down to 
+     * zero, it breaks up.)
+     */
+    public int getStability() 
+    {
+        return stability;
+    }
+    
+    /**
+     * Hit this asteroid dealing the given amount of damage.
+     */
+    public void hit(int damage) 
+    {
+        stability = stability - damage;
+        if (stability <= 0) 
+        {
+            breakUp();
+        }
+    }
+    
+    /**
+     * Break up this asteroid. If we are still big enough, this will create two
+     * smaller asteroids. If we are small already, just disappear.
+     */
+    private void breakUp() 
+    {
+        Greenfoot.playSound("Explosion.wav");
+        
+        if (size <= 16) {
+            getWorld().removeObject(this);
+        }
+        else {
+            int r = getVelocity().getDirection() + Greenfoot.getRandomNumber(45);
+            double l ;
+        }
+    }
+}
